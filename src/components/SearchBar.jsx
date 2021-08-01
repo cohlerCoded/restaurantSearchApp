b3ebc304c6d1eee47f705bd6fd608f69b8912312
cohlerCoded <jeffcohler@cohlercoded.com>
2021-08-01 12:50:47 -0700

    searchbar styling

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,12 +3,19 @@ import { StyleSheet, Text, View } from 'react-native'
 
 const SearchBar = () => {
     return (
-        <View>
+        <View style={styles.background}>
             <Text>Search Bar</Text>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    background:{
+        height:50,
+        backgroundColor:'#e5e5e5',
+        borderRadius: 5,
+        margin:10
+    }
+})
 
 export default SearchBar
