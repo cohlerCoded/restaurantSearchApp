e9d2a78ef44ea1b0a0f92a891f9c70a80f508b28
cohlerCoded <jeffcohler@cohlercoded.com>
2021-08-01 12:30:05 -0700

    SearchBar boilerplate

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const SearchBar = () => {
+    return (
+        <View>
+            <Text>Search Bar</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default SearchBar
