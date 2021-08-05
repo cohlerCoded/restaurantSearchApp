25af88f062a0ff7043a5fc6ff7a21cf57b9cdb12
cohlerCoded <jeffcohler@cohlercoded.com>
2021-08-04 21:36:37 -0700

    showing additional info

--- src/components/ResultDetail.jsx
+++ src/components/ResultDetail.jsx
@@ -4,12 +4,17 @@ import { View, Text, StyleSheet, Image } from 'react-native'
 const ResultDetail = ({ result }) => {
   return (
     <View>
-      <Text>{result.name}</Text>
+      <Image style={styles.imageStyle} source={{ uri: result.image_url }} />
+      <Text style={styles.nameStyle}>{result.name}</Text>
+      <Text>
+        {result.rating} Stars, {result.review_count} Reviews
+      </Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  imageStyle: { width: 250, height: 200, resizeMode: 'stretch' },
+  imageStyle: { width: 250, height: 200, borderRadius: 4 },
+  nameStyle: { fontWeight: 'bold' },
 })
 export default ResultDetail
