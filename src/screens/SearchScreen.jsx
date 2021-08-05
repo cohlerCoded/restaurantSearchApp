3686cbc64bf537710ea5083a2d71f404a0df99fb
cohlerCoded <jeffcohler@cohlercoded.com>
2021-08-04 22:16:59 -0700

    constraining view elements

--- src/screens/SearchScreen.jsx
+++ src/screens/SearchScreen.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { StyleSheet, Text, View, ScrollView } from 'react-native'
-import ImageDetail from '../components/ImageDetail'
 import ResultsList from '../components/ResultsList'
 import SearchBar from '../components/SearchBar'
 import useResults from '../hooks/useResults'
@@ -22,7 +21,7 @@ const SearchScreen = () => {
   )
 
   return (
-    <ScrollView>
+    <View style={{ flex: 1 }}>
       <SearchBar
         term={term}
         onTermChange={setTerm}
@@ -31,32 +30,14 @@ const SearchScreen = () => {
       {errorMessage ? (
         <Text>{errorMessage}</Text>
       ) : (
-        <View>
+        <ScrollView>
           <Text>We found {results.length} results</Text>
           <ResultsList title='Cost Effective' results={costEffective} />
           <ResultsList title='Bit Pricier' results={bitPricier} />
           <ResultsList title='Big Spender' results={bigSpender} />
-          {/* <FlatList
-            showsVerticalScrollIndicator={false}
-            keyExtractor={(business) => business.id}
-            data={results}
-            renderItem={({ item }) => {
-              return (
-                <ImageDetail
-                  title={item.name}
-                  rating={item.rating}
-                  imageURL={
-                    item.image_url
-                      ? item.image_url
-                      : 'https://st4.depositphotos.com/14953852/24787/v/600/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg'
-                  }
-                />
-              )
-            }}
-          /> */}
-        </View>
+        </ScrollView>
       )}
-    </ScrollView>
+    </View>
   )
 }
 
