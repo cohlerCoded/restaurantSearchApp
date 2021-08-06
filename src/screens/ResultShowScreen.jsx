1a195d40fff7b32f284cf2cf978b192d5e5976d0
cohlerCoded <jeffcohler@cohlercoded.com>
2021-08-06 08:33:55 -0700

    get one result api call

--- src/screens/ResultShowScreen.jsx
+++ src/screens/ResultShowScreen.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import WebView from 'react-native-webview'
+import yelp from '../api/yelp'
 
 export default function ResultShowScreen({ navigation }) {
+  const [result, setResult] = useState(null)
+
   const url = navigation.getParam('url')
   const id = navigation.getParam('id')
-  console.log(id)
+
+  console.log(result)
+
+  const getResult = async (id) => {
+    const res = await yelp.get(`/${id}`)
+    setResult(res.data)
+  }
+
+  useEffect(() => {
+    getResult(id)
+  }, [])
   return <WebView source={{ uri: url }} />
 }
 
