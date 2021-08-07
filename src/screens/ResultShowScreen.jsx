95e0fe01a9b3865470771e990444c651d339f2b1
cohlerCoded <jeffcohler@cohlercoded.com>
2021-08-06 21:35:44 -0700

    tinder swipe for photos

--- src/screens/ResultShowScreen.jsx
+++ src/screens/ResultShowScreen.jsx
@@ -1,11 +1,44 @@
-import React, { useState, useEffect } from 'react'
-import { View, Text, StyleSheet, FlatList, Image } from 'react-native'
+import React, { useState, useEffect, useRef, useCallback } from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Image,
+  SafeAreaView,
+} from 'react-native'
 import WebView from 'react-native-webview'
 import Carousel from 'react-native-snap-carousel'
 import yelp from '../api/yelp'
 
+const exampleItems = [
+  {
+    title: 'Item 1',
+    text: 'Text 1',
+  },
+  {
+    title: 'Item 2',
+    text: 'Text 2',
+  },
+  {
+    title: 'Item 3',
+    text: 'Text 3',
+  },
+  {
+    title: 'Item 4',
+    text: 'Text 4',
+  },
+  {
+    title: 'Item 5',
+    text: 'Text 5',
+  },
+]
+
 export default function ResultShowScreen({ navigation }) {
   const [result, setResult] = useState(null)
+  const [activeIndex, setActiveIndex] = useState(0)
+  const [carouselItems, setCarouselItems] = useState(null)
+  const ref = useRef(null)
 
   const url = navigation.getParam('url')
   const id = navigation.getParam('id')
@@ -13,32 +46,83 @@ export default function ResultShowScreen({ navigation }) {
   const getResult = async (id) => {
     const res = await yelp.get(`/${id}`)
     setResult(res.data)
+    setCarouselItems(res.data.photos)
+    console.log(carouselItems)
   }
 
   useEffect(() => {
     getResult(id)
   }, [])
 
-  if (!result) return null
-
+  ////////////WEBVIEW///////////////////////
   // return <WebView source={{ uri: url }} />
-  return (
-    <View>
-      <Text>{result.name}</Text>
-      <FlatList
-        data={result.photos}
-        keyExtractor={(photo) => photo}
-        renderItem={({ item }) => {
-          return <Image style={styles.image} source={{ uri: item }} />
+
+  ////////////////////FLATLIST/////////////////
+  //   return (
+  //     <View>
+  //       <Text>{result.name}</Text>
+  //       <FlatList
+  //         data={result.photos}
+  //         keyExtractor={(photo) => photo}
+  //         renderItem={({ item }) => {
+  //           return <Image style={styles.image} source={{ uri: item }} />
+  //         }}
+  //       />
+  //     </View>
+  //   )
+  // }
+
+  const renderItem = useCallback(
+    ({ item, index }) => (
+      <View
+        style={{
+          backgroundColor: 'floralwhite',
+          borderRadius: 5,
+          height: 300,
+          padding: 10,
+          marginLeft: 15,
+          marginRight: 15,
         }}
-      />
-    </View>
+      >
+        <Image style={styles.image} source={{ uri: item }} />
+      </View>
+    ),
+    []
+  )
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: 'rebeccapurple' }}>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        {result && (
+          <Text style={{ fontSize: 30, position: 'absolute', top: 10 }}>
+            {result.name}
+          </Text>
+        )}
+        <Carousel
+          layout={'tinder'}
+          layoutCardOffset={`4`}
+          ref={ref}
+          data={carouselItems}
+          sliderWidth={300}
+          itemWidth={300}
+          renderItem={renderItem}
+          onSnapToItem={(index) => setActiveIndex(index)}
+        />
+      </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   image: {
-    height: 200,
-    width: 300,
+    height: '100%',
+    width: '100%',
   },
 })
