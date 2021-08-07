4d52bf268bc29897940453d33c6902f3a91d1808
cohlerCoded <jeffcohler@cohlercoded.com>
2021-08-07 10:15:49 -0700

    TinderScreen update

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 import ResultShowScreen from './src/screens/ResultShowScreen'
 import SearchScreen from './src/screens/SearchScreen'
+import TinderScreen from './src/screens/TinderScreen'
 
 const navigator = createStackNavigator(
   {
--- src/screens/TinderScreen.jsx
+++ src/screens/TinderScreen.jsx
@@ -4,6 +4,27 @@ import Swiper from 'react-native-deck-swiper'
 import SearchBar from '../components/SearchBar'
 import useResults from '../hooks/useResults'
 
+const colors = {
+  red: '#EC2379',
+  blue: '#0070FF',
+  gray: '#777777',
+  white: '#ffffff',
+  black: '#000000',
+}
+
+const Card = ({ card }) => {
+  return (
+    <View style={styles.card}>
+      <Image source={{ uri: card.image_url }} style={styles.cardImage} />
+    </View>
+  )
+}
+
+const onSwiped = () => {
+  transitionRef.current.animateNextTransition()
+  setIndex((index + 1) % data.length)
+}
+
 const TinderScreen = () => {
   const [term, setTerm] = useState('')
   const [location, setLocation] = useState('')
@@ -30,11 +51,41 @@ const TinderScreen = () => {
           }
         />
       </View>
-      {errorMessage ? <Text>{errorMessage}</Text> : <View></View>}
+      {errorMessage ? (
+        <Text>{errorMessage}</Text>
+      ) : (
+        <View>
+          {results.length > 0 && (
+            <Swiper
+              cards={results}
+              cardIndex={index}
+              renderCard={(card) => <Card card={card} />}
+              onSwiped={onSwiped}
+            />
+          )}
+        </View>
+      )}
     </>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  card: {
+    flex: 0.45,
+    borderRadius: 8,
+    shadowRadius: 25,
+    shadowColor: colors.black,
+    shadowOpacity: 0.08,
+    shadowOffset: { width: 0, height: 0 },
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.white,
+  },
+  cardImage: {
+    width: 200,
+    flex: 1,
+    resizeMode: 'contain',
+  },
+})
 
 export default TinderScreen
