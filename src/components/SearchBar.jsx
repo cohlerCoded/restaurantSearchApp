4fc8246c231651a844ad4d7e6dd2fd2842c0dfc9
cohlerCoded <jeffcohler@cohlercoded.com>
2021-08-07 00:03:33 -0700

    fomatting change

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,42 +1,42 @@
 import React from 'react'
 import { StyleSheet, TextInput, View } from 'react-native'
-import { Feather } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons'
 
-const SearchBar = ({term,onTermChange, onTermSubmit}) => {
-    return (
-        <View style={styles.backgroundStyle}>
-            <Feather name="search" style={styles.searchIcon}/>
-            <TextInput 
-                autoCapitalize='none'
-                autoCorrect={false}
-                placeholder='Search' 
-                style={styles.searchInput} 
-                value={term} 
-                onChangeText={onTermChange}
-                onEndEditing={onTermSubmit}
-            />
-        </View>
-    )
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
+  return (
+    <View style={styles.backgroundStyle}>
+      <Feather name='search' style={styles.searchIcon} />
+      <TextInput
+        autoCapitalize='none'
+        autoCorrect={false}
+        placeholder='Search'
+        style={styles.searchInput}
+        value={term}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
+      />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    backgroundStyle:{
-        height:50,
-        backgroundColor:'#e5e5e5',
-        borderRadius: 5,
-        margin:15,
-        flexDirection:'row',
-    },
-    searchInput:{
-        flex:1,
-        fontSize:18,
-    },
-    searchIcon:{
-        color:'black',
-        fontSize:36,
-        alignSelf:'center',
-        marginHorizontal:5,
-    }
+  backgroundStyle: {
+    height: 50,
+    backgroundColor: '#e5e5e5',
+    borderRadius: 5,
+    margin: 15,
+    flexDirection: 'row',
+  },
+  searchInput: {
+    flex: 1,
+    fontSize: 18,
+  },
+  searchIcon: {
+    color: 'black',
+    fontSize: 36,
+    alignSelf: 'center',
+    marginHorizontal: 5,
+  },
 })
 
 export default SearchBar
